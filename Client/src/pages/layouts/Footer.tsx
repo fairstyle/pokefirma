4db33f94abf472abe5172a7194e340321ded35a1
Andres Villarroel <kuvickdabest@gmail.com>
2024-06-01 21:20:57 -0400

    feat: layouts

--- Client/src/pages/layouts/Footer.tsx
+++ Client/src/pages/layouts/Footer.tsx
@@ -1,3 +1,5 @@
 export const FooterPage = () => {
-    return (<div>footer page</div>)
+    return (<footer className="bg-gray-100 px-4 py-2 text-center">
+        <span className="italic text-gray-500">Prueba tecnica para postular a desarrollador fullstack en firmavirtual.legal</span>
+    </footer>)
 }
--- Client/src/pages/layouts/Header.tsx
+++ Client/src/pages/layouts/Header.tsx
@@ -1,3 +1,19 @@
 export const HeaderPage = () => {
-    return (<div>header</div>)
+    return (<header className="bg-gray-100 px-4 py-2 drop-shadow-md">
+        <div className="flex justify-between">
+            <div className="w-fit">
+                <a href="/pokemon"><h1 className="text-3xl font-black">Pokefirma</h1></a>
+            </div>
+            <div className="flex space-x-4">
+                <div className="hidden md:block self-center">
+                    <input className="rounded-lg bg-white w-64 p-2 focus:outline-gray-300 text-gray-400" type="text" placeholder="Buscar pokemon por nombre" />
+                </div>
+                <div className="self-center hover:bg-gray-200 p-1 rounded-lg border border-dashed border-gray-300 cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+                    </svg>
+                </div>
+            </div>
+        </div>
+    </header>)
 }
